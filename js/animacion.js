921317a44d3480e7ab16d13aa73ca1ce788297a5
Cristianps <cristian@MacBook-Air-de-Cristian.local>
2022-11-06 17:30:12 -0500

    Ubication, menu blocked and scroll transitions added

---
+++ js/animacion.js
@@ -0,0 +1,37 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.cabecero', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.nosotros', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.galeria', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.servicios', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.ubicacion', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.contacto', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
