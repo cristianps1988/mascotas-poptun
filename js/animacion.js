db0ba169619270840c9a4b7b68db79c080b0aed9
Cristianps <cristian@MacBook-Air-de-Cristian.local>
2022-11-06 18:47:36 -0500

    animation errors fixed

--- js/animacion.js
+++ js/animacion.js
@@ -1,37 +1,48 @@
 window.sr = ScrollReveal();
 
-    sr.reveal('.cabecero', {
-        duration: 3000,
-        origin: 'bottom',
-        distance: '-100px'
+    sr.reveal('#anim-left', {
+        duration: 3500,
+        origin: 'left',
+        distance: '200px'
+    });
+
+    sr.reveal('#anim-right', {
+        duration: 3500,
+        origin: 'right',
+        distance: '200px'
     });
 
     sr.reveal('.nosotros', {
-        duration: 3000,
+        duration: 3500,
         origin: 'bottom',
-        distance: '-100px'
+        distance: '200px'
+    });
+
+    sr.reveal('.imagen-galeria', {
+        duration: 2500,
     });
 
     sr.reveal('.galeria', {
-        duration: 3000,
+        duration: 3500,
         origin: 'bottom',
-        distance: '-100px'
     });
 
     sr.reveal('.servicios', {
-        duration: 3000,
+        duration: 3500,
+        origin: 'bottom'
+    });
+
+    sr.reveal('.servicio-individual', {
+        duration: 2500,
         origin: 'bottom',
-        distance: '-100px'
     });
 
     sr.reveal('.ubicacion', {
-        duration: 3000,
+        duration: 3500,
         origin: 'bottom',
-        distance: '-100px'
     });
 
     sr.reveal('.contacto', {
-        duration: 3000,
+        duration: 3500,
         origin: 'bottom',
-        distance: '-100px'
     });
